7dc9a9039302415991a5231bd588b081ea7a01bd
Godsizee <badesebastian@outlook.com>
2026-06-08 13:06:25 +0200

    1

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -15,6 +15,8 @@
 
 	let incomeInput = $state('');
 	let savingIncome = $state(false);
+	let budgetInput = $state('');
+	let savingBudget = $state(false);
 
 	let oldPassword = $state('');
 	let newPassword = $state('');
